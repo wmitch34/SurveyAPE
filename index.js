4a9e02291a0b37ecd069028fd1768e3f80aac559
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-11-21 16:34:53 -0500

    typosssss

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
 function login(){
-    let email = Document.getElementbyID("logEmail");
-    let password = Document.getElementbyID("logPass");
-    let loginReq = {
+    let email = document.getElementbyID("logEmail");
+    let password = document.getElementbyID("logPass");
+    let loginObj = {
         email: email,
         password: password     
     }
-    sendLoginRequest(loginReq);
+    let loginReq = sendLoginRequest(loginObj);
+    if(loginReq === -1){
+        // error message
+    }else{
+        // set cookie
+        // redirect to user page
+    }
 }
 
 function register(){
+    let email = document.getElementbyID("logEmail");
+    let password = document.getElementbyID("logPass");
+    let verifiedPassword = document.getElementbyID("regPassConfirm");
+
+    if(pasword === verifiedPassword){
+        let regObj = {
+            email: email,
+            password: password     
+        }
+        let regReq = sendLoginRequest(regObj);
+
+        if(loginReq === -1){
+            // error message
+            // possibly bad request or email already in use?
+        }else{
+            // redirect to login prompt
+            // show account created message
+        }
+
+    }
+    
 
 }
 
 function showRegisterForm(){
     document.getElementById("registerPrompt").style = "display: block"
     document.getElementById("loginPrompt").style = "display: none"
-
 }
 
 function cancelRegister(){
     document.getElementById("registerPrompt").style = "display: none"
     document.getElementById("loginPrompt").style = "display: block"
-
-
 }
