3d12f2006140a0ae7f33b46ef246f923eda3a663
Michael Mederos <mikemederos01@gmail.com>
2022-11-30 00:38:41 -0500

    Setting up JSON intake on PHP

--- php/register.php
+++ php/register.php
@@ -1,1 +1,5 @@
 <!-- add a user to db if it does not already exist. Return negative message for failure, positive mesage for success -->
+<?php
+    $str_regObj = file_get_contents('php://input'); //receives raw data from HTTP request
+    $data = json_decode($str_regObj);   //decodes JSON into PHP object
+?>
