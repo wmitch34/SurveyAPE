97b7f608dec1049f77f0189c5664a89991473e97
Javi-Vargas <jvargas_14@knights.ucf.edu>
2022-12-03 01:28:37 -0500

    Updated register and getMySurveys
    Modified them to work with our site and its databases.

--- php/getMySurveys.php
+++ php/getMySurveys.php
@@ -1,1 +1,54 @@
 <!-- return array of surveys that curr user has created -->
+<?php
+	$exist = True;
+
+	$inData = getRequestInfo();
+    $Email = $indDatata["Email"];
+	//$CheckUser = "SELECT * from Users WHERE Email = '$Email'";
+	$conn = new mysqli("localhost", "root@localhost", "Group18Aa", "Group18");	//*****
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$CheckQuery = mysqli_query($conn,$CheckUser);
+		$stmt = $conn->prepare("SELECT title FROM surveys WHERE Email='$Email'");
+		$stmt->execute();
+		$result = $stmt->get_result();
+		if( $row = $result->fetch_assoc())
+		{
+			returnWithInfo( $row['email']);
+		}
+		else
+		{
+			returnWithError("Unexpected error");
+		}
+		$stmt->close();
+		$conn->close();
+   }
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithInfo($result)
+	{
+		$retValue = '{"ID":' . $result . '}';
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
