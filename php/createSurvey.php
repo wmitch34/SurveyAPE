1f322bf5a53f0a11350303de3f309f25eed36d60
Michael Mederos <mikemederos01@gmail.com>
2022-12-03 23:32:04 -0500

    Update createSurvey.php

--- php/createSurvey.php
+++ php/createSurvey.php
@@ -1,23 +1,28 @@
 <!-- add created survey to database -->
+
 <?php
 	$inData = getRequestInfo();
-	$title = $inData["title"];
-	$type = $inData["type"];
-    $description = $inData["description"];
-    $question = $inData["question"];
-
-	$conn = new mysqli("localhost", "root@localhost", "Group18Aa", "Group18");	//*****
+	echo $inData["title"];
+	$title = $inData["title"];						//title of survey
+	$desc = $inData["desc"]; 						//survey desc
+	$ownerEmail = $inData["ownerEmail"];			//owners email
+	#$paEmails = $inData["pEmails"];							//array of JSON objects consisting of participant emails
+	#$question = $inData["question"];							//array of JSON objects consisting of string & type
+	$conn = new mysqli("localhost", "will", "dbscrub", "Group18");	
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		//$stmt = $conn->prepare("UPDATE Contacts ID SET Name = ?, Phone =?, Email = ? where ID=?");
-        $stmt = $conn->prepare("INSERT into surveys (title, description, question, type) VALUES(?,?,?,?)");
-        //$stmt = $conn->prepare("UPDATE into Contacts (UserId,Name,Phone, Email) VALUES(?,?,?,?)");
-		//$stmt->bind_param("ssss", $userId, $Name, $Phone, $Email);
-        $stmt->bind_param("ssss",$title, $description, $question, $type);  //if s's are string then i could be int and it could be sssi
+        $stmt = $conn->prepare("INSERT into surveys (`surveyID`, `title`, `description`, `email`) VALUES(NULL,?,?,?)");
+        $stmt->bind_param("sss", $title, $desc, $ownerEmail);
+		
+		$stmt1 = $conn->query("SELECT LAST_INSERT_ID()");
+		
+
+		$stmt1 = $conn->prepare("INSERT into questions (`surveyID`,`pEmails`,`question, `answer`, `type`) VALUES(NULL,?,?,?,?,?)");
+        $stmt1->bind_param("isss",$surveyID, $pEmails, $question, $answer, $type);  //if s's are string then i could be int and it could be sssi
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
@@ -26,6 +31,7 @@
 
 	function getRequestInfo()
 	{
+		//return json_decode(file_get_contents('php://input'), true);
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
