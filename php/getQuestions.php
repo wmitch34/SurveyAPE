e292751b03c578a6cd32b2b8c572e4f169e1df1c
Michael Mederos <mikemederos01@gmail.com>
2022-12-04 19:05:43 -0500

    Update getQuestions.php

--- php/getQuestions.php
+++ php/getQuestions.php
@@ -1,15 +1,13 @@
 <?php	//return array of surveys that curr user has created
 	$inData = json_decode(file_get_contents('php://input'), true);
-    $email = $inData["email"];
 	$surveyID = $inData["surveyID"];	//surveyID of person logged in
 	$conn = new mysqli("localhost", "will", "dbscrub", "Group18");
 	if($conn->connect_error){
 		returnWithError($conn->connect_error);
 	}
 	else{
-		//$stmt = $conn->prepare("SELECT `title`, `description`, `type`  FROM surveys, questions WHERE surveys.surveyID = ? AND surveys.surveyID = questions.surveyID");
-		$stmt = $conn->prepare("SELECT `question`, `answer` FROM questions WHERE surveyID = ? AND participantEmail = ?");
-		$stmt->bind_param("is", $surveyID, $email);
+		$stmt = $conn->prepare("SELECT `question`, group_concat(`answer`) AS answer FROM questions WHERE surveyID = ? GROUP BY question");
+		$stmt->bind_param("i", $surveyID);
 		$stmt->execute();
 		$result = $stmt->get_result();
         $rows = array();
